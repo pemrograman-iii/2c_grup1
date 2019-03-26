6d048e08ae1ce1414393ec2f049ff15a06bc65ac
ilhammariq <ilhammuhammadariq123@gmail.com>
2019-03-26 21:23:45 +0700

    Bismillah Ariq

---
+++ src/4/1174087/Praktek/main2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 18:49:05 2019
+
+@author: PandA23
+"""
+
+lib = __import__('1174087_csv')
+
+lib.modelistcsv()
+lib.modediccsv()
+lib.menuliscsv()
