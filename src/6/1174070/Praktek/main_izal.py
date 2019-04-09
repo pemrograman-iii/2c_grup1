0390de3e53e119347285342937692d3cde7f8854
Arrizal <solehizal@gmail.com>
2019-04-10 05:26:55 +0700

    izal prak6

---
+++ src/6/1174070/Praktek/main_izal.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 22:45:04 2019
+
+@author: User
+"""
+
+import p1174070_bar as bar
+import p1174070_scatter as sct
+import p1174070_pie as pie
+import p1174070_plot as plt
+
+bar.batang()
+sct.titik()
+pie.pie()
+plt.plot()
